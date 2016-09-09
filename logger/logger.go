ca91bf5baa0cf20e543d012fd799366a6253db64
Fernando Moro Hernández <fernando@MacBook-Pro-de-Fernando.local>
2016-09-09 18:52:10 +0200

    chg: dev: fixed logger typos

--- logger/logger.go
+++ logger/logger.go
@@ -1,24 +1,24 @@
 package logger
 
-import log "github.com/Sirupsen/logrus"
+import logrus "github.com/Sirupsen/logrus"
 
 // Log is an instance of the logrus logger to be used across the application
 var Log = log.New()
 
 func init() {
-	Logger.Level = log.DebugLevel
+	Log.Level = log.DebugLevel
 }
 
 // FileCreated logs the successfull creation of a file
 func FileCreated(path string) {
-	Logger.WithFields(log.Fields{
+	Log.WithFields(logrus.Fields{
 		"path": path,
 	}).Info("Successfully created")
 }
 
 // CommandCall logs the execution of a cli command
 func CommandCall(command string, args []string) {
-	Logger.WithFields(log.Fields{
+	Log.WithFields(logrus.Fields{
 		"command": command,
 		"args":    args,
 	}).Info("Executing command")
